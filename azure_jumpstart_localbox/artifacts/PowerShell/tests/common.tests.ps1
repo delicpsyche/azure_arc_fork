2b6afbc5e1683cf336094027c0a77b0c62e5d326
delicpsyche <delicpsyche@outlook.com>
2026-01-31 16:47:28 +0530

    Initial commit: Fix Az.Resources module import issues and update repo references

---
+++ azure_jumpstart_localbox/artifacts/PowerShell/tests/common.tests.ps1
@@ -0,0 +1,29 @@
+BeforeDiscovery {
+
+    # Import Azure PowerShell modules
+    Import-Module Az.Resources -Force
+    Import-Module Az.Accounts -Force
+
+    $null = Connect-AzAccount -Identity -Tenant $env:tenantId -Subscription $env:subscriptionId
+
+}
+
+if ("True" -eq $env:autoDeployClusterResource) {
+Describe "LocalBox resource group" {
+    BeforeAll {
+        $ResourceGroupName = $env:resourceGroup
+    }
+    It "should have 25 resources or more" {
+        (Get-AzResource -ResourceGroupName $ResourceGroupName).count | Should -BeGreaterOrEqual 25
+    }
+}
+} else {
+Describe "LocalBox resource group" {
+    BeforeAll {
+        $ResourceGroupName = $env:resourceGroup
+    }
+    It "should have 18 resources or more" {
+        (Get-AzResource -ResourceGroupName $ResourceGroupName).count | Should -BeGreaterOrEqual 18
+    }
+}
+}
